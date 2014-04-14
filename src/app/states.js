5053a1c863320e9b2bab35c2584630e8b4343a2b
Alex Tharp <alex.tharp@careerbuilder.com>
2014-04-14 14:43:53 -0400

    Add .editorconfig, remove bogus states

--- src/app/states.js
+++ src/app/states.js
@@ -1,50 +1,8 @@
-var module = angular.module('x.states', [
+var module = angular.module('app.states', [
   'ui.router.state',
   'ui.router.router',
-  'x.templates',
-  'x.controllers.advice-and-resources',
-  'x.controllers.post',
-  'x.services.postTransformer'
+  'app.templates'
 ]);
 
 module.config(function($stateProvider) {
-
-  $stateProvider
-    .state('x', {
-      abstract:    true,
-      url:         '',
-      templateUrl: 'project-x.tpl.html'
-    })
-
-    .state('x.advice-and-resources', {
-      abstract:    true,
-      url:         '/advice-and-resources',
-      templateUrl: 'advice-and-resources/advice-and-resources.tpl.html'
-    })
-
-    .state('x.advice-and-resources.home', {
-      url:          '',
-      templateUrl:  'advice-and-resources/home.tpl.html',
-      data: {title: 'Advice & Resources'},
-      controller:   'AdviceAndResourcesCtrl'
-    })
-
-    .state('x.advice-and-resources.post', {
-      url:         '/posts/:id',
-      templateUrl: 'advice-and-resources/post.tpl.html',
-      data:        {title: 'Post'},
-      controller:  'PostCtrl',
-      resolve: {
-        post: function(cortex, postTransformer, $stateParams, $q) {
-          var d = $q.defer();
-
-          cortex.posts.get({id: $stateParams.id}).$promise.then(function(post) {
-            postTransformer.transform(post);
-            d.resolve(post);
-          });
-
-          return d.promise;
-        }
-      }
-    });
 });
